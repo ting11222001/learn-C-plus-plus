12542c5c68f397d94003996a21f6b861bf6f262c
tiffany <ting11222001@gmail.com>
2023-03-08 14:27:30 +1030

    feat: 🎸 check the range of int data type

--- Variables.cpp
+++ Variables.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include <climits>
 
 int main()
 {
@@ -12,6 +13,10 @@ int main()
 
   // use sizeof to check the memory space a data type is taking
   cout << "Size of int is " << sizeof(int) << " bytes\n"; // output: Size of int is 4 bytes
+  // check the min of int and the max of int
+  // remember to add "#include <climits>"
+  cout << "Int min value is " << INT_MIN << endl; // -1, -2, -3, ..., -2147483648
+  cout << "Int max value is " << INT_MAX << endl; // 0, +1, +2, +3, ..., +2147483647
 
   return 0;
 }
