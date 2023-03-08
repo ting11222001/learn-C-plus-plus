6fe781c16aa2f82412fb870c11c931ce3226021e
tiffany <ting11222001@gmail.com>
2023-03-08 14:32:41 +1030

    feat: 🎸 check other data types memory space

--- Variables.cpp
+++ Variables.cpp
@@ -12,11 +12,21 @@ int main()
   double balance = 4567982;
 
   // use sizeof to check the memory space a data type is taking
-  cout << "Size of int is " << sizeof(int) << " bytes\n"; // output: Size of int is 4 bytes
+  cout << "Size of int is " << sizeof(int) << " bytes\n"; // Size of int is 4 bytes
   // check the min of int and the max of int
   // remember to add "#include <climits>"
   cout << "Int min value is " << INT_MIN << endl; // -1, -2, -3, ..., -2147483648
   cout << "Int max value is " << INT_MAX << endl; // 0, +1, +2, +3, ..., +2147483647
+
+  // check the unsigned int
+  cout << "Size of unsigned int is " << sizeof(unsigned int) << " bytes\n"; // Size of unsigned int is 4 bytes
+  cout << "Unsigned int max value is " << UINT_MAX << endl;
+  // Unsigned int max value is 4294967295
+
+  cout << "Size of bool is " << sizeof(bool) << " bytes\n";     // Size of bool is 1 bytes
+  cout << "Size of char is " << sizeof(char) << " bytes\n";     // Size of char is 1 bytes
+  cout << "Size of float is " << sizeof(float) << " bytes\n";   // Size of float is 4 bytes
+  cout << "Size of double is " << sizeof(double) << " bytes\n"; // Size of double is 8 bytes
 
   return 0;
 }
