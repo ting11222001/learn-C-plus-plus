6d1d764126bc95048b5375d2e16b900277211efc
tiffany <ting11222001@gmail.com>
2023-03-08 13:48:55 +1030

    feat: 🎸 create for loops

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,6 +6,14 @@ using namespace std;
 int main()
 {
   cout << "Hello world!" << endl;
+
+  string fruit = "apple";
+  string fruits[5] = {"apple", "organge", "pear", "strawberry", "banana"};
+  for (int i = 0; i < 5; i++)
+  {
+    cout << fruits[i] << endl;
+  }
+
   return 0;
 }
 
