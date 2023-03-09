7f2775cb38dc57ed4073b8132cd5ed85e13f6736
Tiffany Liao <tiffany@trunk-studio.com>
2023-03-09 11:52:54 +1030

    feat: 🎸 try a while loop

--- InfiniteLoop.cpp
+++ InfiniteLoop.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
+  // write out all the numbers between 100-500 that are divisible by 3 and 5
+  int counter = 100;
+
+  while (counter <= 500)
+  {
+    if (counter % 3 == 0 && counter % 5 == 0)
+    {
+      cout << counter << " is divisible." << endl;
+    }
+    counter++;
+  }
 
   return 0;
 }
