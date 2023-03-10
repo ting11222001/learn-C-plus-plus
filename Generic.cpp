9755a81b602c4931c7281fd62ceb22c0803300f2
tiffany <ting11222001@gmail.com>
2023-03-10 12:08:16 +1030

    feat: 🎸 use generic functions

--- Generic.cpp
+++ Generic.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
 using namespace std;
 
-// & means passing in the reference of a and b
-void swap(int &a, int &b)
+template <typename T> // T means generic data type. It can be any name e.g. Type
+
+// generic function's names needs to be captialized
+void Swap(T &a, T &b)
 {
-  int temp = a;
+  T temp = a;
   a = b;
   b = temp;
 }
 
-void swap(char &a, char &b)
-{
-  char temp = a;
-  a = b;
-  b = temp;
-}
+// overload approach
+// // & means passing in the reference of a and b
+// void swap(int &a, int &b)
+// {
+//   int temp = a;
+//   a = b;
+//   b = temp;
+// }
+
+// void swap(char &a, char &b)
+// {
+//   char temp = a;
+//   a = b;
+//   b = temp;
+// }
 
 int main()
 {
   int a = 5, b = 7;
   cout << a << " - " << b << endl;
-  swap(a, b);
+  Swap(a, b);
+  // Swap<int>(a, b); // I can invoke specific data type
   cout << a << " - " << b << endl;
 
   char c = 'c', d = 'd';
   cout << c << " - " << d << endl;
-  swap(c, d);
+  Swap(c, d);
+  // Swap<char>(a, b);
   cout << c << " - " << d << endl;
 
   return 0;
