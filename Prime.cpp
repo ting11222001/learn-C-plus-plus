68a4151fef9954369ba963986eed715e61c495a7
tiffany <ting11222001@gmail.com>
2023-03-10 11:08:45 +1030

    feat: 🎸 try function reusability

--- Prime.cpp
+++ Prime.cpp
@@ -19,19 +19,28 @@ int main()
   // check if a number is a prime number
   // i.e. if it's divisible by other than 1 and itself
 
-  int number;
-  cout << "Number: ";
-  cin >> number;
+  // int number;
+  // cout << "Number: ";
+  // cin >> number;
 
-  bool isPrimeFlag = isPrimeNumber(number);
+  // bool isPrimeFlag = isPrimeNumber(number);
 
-  if (isPrimeFlag)
+  // if (isPrimeFlag)
+  // {
+  //   cout << "Prime number" << endl;
+  // }
+  // else
+  // {
+  //   cout << "Not prime number" << endl;
+  // }
+
+  // We can use the same function in other scenarios
+  // e.g. list out all the prime numbers of a range
+  for (int i = 1; i <= 30; i++)
   {
-    cout << "Prime number" << endl;
-  }
-  else
-  {
-    cout << "Not prime number" << endl;
+    bool isPrime = isPrimeNumber(i);
+    if (isPrime)
+      cout << i << " is prime number\n";
   }
 
   return 0;
