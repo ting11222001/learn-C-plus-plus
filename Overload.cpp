bf57e475838125d7340ca5095f146216818c4b61
tiffany <ting11222001@gmail.com>
2023-03-10 11:15:13 +1030

    feat: 🎸 function overloading

---
+++ Overload.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b);
+double sum(double a, double b);
+float sum(float a, float b, float c);
+
+int main()
+{
+  // hover on sum(): to see the hint: we have three overload functions
+  cout << sum(4, 3) << endl;          // 7
+  cout << sum(4.4, 3.3) << endl;      // 7.7
+  cout << sum(4.4, 3.3, 2.2) << endl; // 9.9
+  return 0;
+}
+
+int sum(int a, int b)
+{
+  return a + b;
+}
+
+double sum(double a, double b)
+{
+  return a + b;
+}
+
+float sum(float a, float b, float c)
+{
+  return a + b + c;
+}
