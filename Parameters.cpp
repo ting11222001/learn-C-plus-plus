19d72791a91b90d9171e9e7e3003982ab08ed85f
tiffany <ting11222001@gmail.com>
2023-03-10 10:47:33 +1030

    feat: 🎸 try taking user input

--- Parameters.cpp
+++ Parameters.cpp
@@ -17,10 +17,25 @@ void introduceMe(string name, int age, string city = "NA", int count = 0) // par
 
 int main()
 {
-  introduceMe("Apple", 20, "Adelaide", 3);
-  // My name is Apple. My age is 20. I live in Adelaide. I have 3 laptops.
-  introduceMe("Banana", 5);
-  // My name is Banana. My age is 5. I live in NA.
+  // introduceMe("Apple", 20, "Adelaide", 3);
+  // // My name is Apple. My age is 20. I live in Adelaide. I have 3 laptops.
+  // introduceMe("Banana", 5);
+  // // My name is Banana. My age is 5. I live in NA.
+
+  // take user input
+  string name, city;
+  int age, count;
+  cout << "Name: ";
+  cin >> name;
+  cout << "Age: ";
+  cin >> age;
+  cout << "City: ";
+  cin >> city;
+  cout << "How many laptop do you have: ";
+  cin >> count;
+
+  introduceMe(name, age, city, count);
+  introduceMe(name, age); // city and count will be default values
 
   return 0;
 }
