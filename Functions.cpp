41bc00a327e3b90476a50727eba25d917a7a8cbd
tiffany <ting11222001@gmail.com>
2023-03-10 10:30:11 +1030

    feat: 🎸 try functions

---
+++ Functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void function(); // we can right click on this to go to definition.
+
+// void function() // It needs to be on top of the main function, or use function declaration at the top.
+// {
+//   cout << "Hello from function()\n";
+// }
+
+int main()
+{
+  cout << "Hello from main()\n"; // always print: Hello from main()
+  function();                    // print when called: Hello from function()
+
+  return 0;
+}
+
+void function()
+{
+  cout << "Hello from function()\n";
+}
