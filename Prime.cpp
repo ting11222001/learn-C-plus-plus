2a4b8a0b7156fa77159bb14fedd0ecf1ceea4a21
tiffany <ting11222001@gmail.com>
2023-03-10 10:58:07 +1030

    feat: 🎸 create check prime program

---
+++ Prime.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void introduceMe()
+{
+}
+
+int main()
+{
+  // check if a number is a prime number
+  // i.e. if it's divisible by other than 1 and itself
+
+  int number;
+  cout << "Number: ";
+  cin >> number;
+
+  bool isPrimeFlag = true;
+  for (int i = 2; i < number; i++)
+  {
+    if (number % i == 0)
+    {
+      isPrimeFlag = false;
+      break;
+    }
+  }
+
+  if (isPrimeFlag)
+  {
+    cout << "Prime number" << endl;
+  }
+  else
+  {
+    cout << "Not prime number" << endl;
+  }
+
+  return 0;
+}
