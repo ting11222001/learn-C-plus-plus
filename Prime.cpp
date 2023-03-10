b77018d0804465dda3a1024b231939cfb24813ba
tiffany <ting11222001@gmail.com>
2023-03-10 11:01:51 +1030

    refactor: 💡 create a bool function out of main

--- Prime.cpp
+++ Prime.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main()
+bool isPrimeNumber(int number)
 {
-  // check if a number is a prime number
-  // i.e. if it's divisible by other than 1 and itself
-
-  int number;
-  cout << "Number: ";
-  cin >> number;
-
   bool isPrimeFlag = true;
   for (int i = 2; i < number; i++)
   {
@@ -19,6 +12,20 @@ int main()
       break;
     }
   }
+
+  return isPrimeFlag;
+}
+
+int main()
+{
+  // check if a number is a prime number
+  // i.e. if it's divisible by other than 1 and itself
+
+  int number;
+  cout << "Number: ";
+  cin >> number;
+
+  bool isPrimeFlag = isPrimeNumber(number);
 
   if (isPrimeFlag)
   {
